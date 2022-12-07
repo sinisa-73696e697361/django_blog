c979094e3b16fb0c2876a63f54d8e4820b66e7ff
sinisaninkovic <sinisa.ninkovic01@gmail.com>
2022-12-07 17:35:23 +0100

    users app

--- django_blog_project/users/urls.py
+++ django_blog_project/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
     path('account/', views.userAccount, name='account'),
     path('edit-account/', views.editAccount, name='edit-account'),
+    path('create-skill/', views.createSkill, name='create-skill'),
 ]
